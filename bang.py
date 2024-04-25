52a9c119b5512609cf077ccda8bb3c5ce42a1342
The-Ellemist <133489227+The-Ellemist@users.noreply.github.com>
2024-04-26 09:30:35 +1000

    fixed up some errors

--- bang.py
+++ bang.py
@@ -17,6 +17,7 @@ class BangGame:
         self.player_1_reload = 0
         self.player_2_reload = 0
         self.score = 0
+        self.number_of_games = 1
 
     def reset(self):
         self.player_1_shields = 5
@@ -26,66 +27,99 @@ class BangGame:
         self.score = 0
 
     def play_step(self, player_1_action, player_2_action):
-        print("Player 1 shields")
-        print(self.player_1_shields)
-        print("Player 1 reload")
-        print(self.player_1_reload)
-        print()
-        print("Player 2 shields")
-        print(self.player_2_shields)
-        print("Player 2 reload")
-        print(self.player_2_reload)
-        print()
+        pri = self.number_of_games / 10 == int(self.number_of_games / 10)
+        pri = True
+        if pri:
+            print(f"Player 1 shields: {self.player_1_shields}")
+            print(f"Player 1 reload: {self.player_1_reload}")
+            print(f"Player 2 shields: {self.player_2_shields}")
+            print(f"Player 2 reload: {self.player_2_reload}")
+            print()
         reward = 0
         gameover = False
-        self.score += 1
         if player_1_action[1] == 1:
-            print("Player 1 reloaded")
+            if pri:
+                print("Player 1 reloaded")
             self.player_1_reload = 1
             self.player_1_shields = 5
             if player_2_action[0] == 1:
-                print("Player 2 shielded")
+                if pri:
+                    print("Player 2 shielded")
                 self.player_2_shields -= 1
             elif player_2_action[1] == 1:
-                print("Player 2 reloaded")
+                if pri:
+                    print("Player 2 reloaded")
                 self.player_2_reload = 1
                 self.player_2_shields = 5
             elif player_2_action[2] == 1:
-                print("Player 2 shot")
+                if pri:
+                    print("Player 2 shot")
                 if self.player_2_reload == 1:
                     reward -= 10
                     gameover = True
         elif player_1_action[0] == 1:
-            print("Player 1 shielded")
+            if pri:
+                print("Player 1 shielded")
             self.player_1_shields -= 1
             if player_2_action[0] == 1:
-                print("Player 2 shielded")
+                if pri:
+                    print("Player 2 shielded")
                 self.player_2_shields -= 1
             elif player_2_action[1] == 1:
-                print("Player 2 reloaded")
+                if pri:
+                    print("Player 2 reloaded")
                 self.player_2_reload = 1
                 self.player_2_shields = 5
             elif player_2_action[2] == 1:
-                print("Player 2 shot")
+                if pri:
+                    print("Player 2 shot")
                 self.player_2_reload = 0
                 self.player_2_shields = 5
-                #reward += 1
+                reward += 1
         elif player_1_action[2] == 1:
-            print("Player 1 shot")
+            if pri:
+                if self.player_1_reload == 1:
+                    print("Player 1 shot")
+                else:
+                    print("Player 1 tried to shoot")
             if player_2_action[0] == 1:
-                print("Player 2 shielded")
+                if pri:
+                    print("Player 2 shielded")
                 self.player_2_shields -= 1
             elif player_2_action[1] == 1:
-                print("Player 2 reloaded")
-                reward += 20
-                gameover = True
+                if pri:
+                    print("Player 2 reloaded")
+                if self.player_1_reload == 1:
+                    reward += 20
+                    score = 1
+                    gameover = True
+                else:
+                    self.player_2_reload = 1
+                    self.player_2_shields = 5
             elif player_2_action[2] == 1:
-                print("Player 2 shot")
-                self.player_2_shields = 5
-                self.player_2_reload = 0
-            self.player_1_shields = 5
-            self.player_1_reload = 0
-        print("-----------")
+                if pri:
+                    if self.player_2_reload == 1:
+                        print("Player 2 shot")
+                    else:
+                        print("Player 2 tried to shoot")
+                if self.player_2_reload == 1:
+                    self.player_2_shields = 5
+                    self.player_2_reload = 0
+                    if self.player_1_reload == 0:
+                        reward -= 10
+                        gameover = True
+                else:
+                    if self.player_1_reload == 1:
+                        reward += 20
+                        score = 1
+                        gameover = True
+            if self.player_1_reload == 1:
+                self.player_1_shields = 5
+                self.player_1_reload = 0
+            else:
+                reward -= 3
+        if pri:
+            print("-----------")
         if gameover:
-            self.score = -self.score
+            self.number_of_games += 1
         return reward, gameover, self.score
--- bangagent.py
+++ bangagent.py
@@ -13,12 +13,12 @@ LR = 0.001
 
 class Agent:
 
-    def __int__(self):
+    def __init__(self):
         self.n_games = 0
         self.epsilon = 0  # randomness
         self.gamma = 0.9  # discount rate
         self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
-        self.model = Linear_QNet(11, 256, 3)
+        self.model = Linear_QNet(3, 256, 3)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
     def get_state(self, game, player_id):
@@ -57,7 +57,7 @@ class Agent:
     def get_action(self, state):
         # random moves: tradeoff exploration / exploitation
         self.epsilon = 80 - self.n_games
-        final_move = [0,0,0]
+        final_move = [0, 0, 0]
         if random.randint(0, 200) < self.epsilon:
             move = random.randint(0, 2)
             final_move[move] = 1
@@ -70,7 +70,7 @@ class Agent:
         return final_move
 
 
-def train()
+def train():
     plot_scores = []
     plot_mean_scores = []
     total_score = 0
@@ -86,7 +86,7 @@ def train()
 
         # opponent move
         opponent_move = [0, 0, 0]
-        opponent_move[random.randint(0, 2)] = 1
+        opponent_move[int(input(">>>"))] = 1
 
         # perform move and get new state
         reward, done, score = game.play_step(final_move, opponent_move)
@@ -115,3 +115,6 @@ def train()
             mean_score = total_score / agent.n_games
             plot_mean_scores.append(mean_score)
             plot(plot_scores, plot_mean_scores)
+
+
+train()
